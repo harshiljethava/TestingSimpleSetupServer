79d225825d196e2e06697bef90654bdabbb7155f
Harshil Jethava <harshiljethava7@gmai.com>
2016-04-11 00:04:09 +0530

    added new code for phpmyadmin :257

--- sss/cli/plugins/stack.py
+++ sss/cli/plugins/stack.py
@@ -255,20 +255,17 @@ class SSSStackController(CementBaseController):
 
             #phpmyadmin config
             if set(SSSVariables.sss_pma).issubset(set(apt_packages)):
-                """if not (os.path.isfile('/etc/apache2/apache2.conf')):
-                    data = dict(webroot=SSSVariables.sss_webroot)
-                    php_conf = ("Include /etc/phpmyadmin/apache.conf")
+                php_conf = ("Include /etc/phpmyadmin/apache.conf")
+                with open('/etc/apache2/apache2.conf', 'a') as php_conf_file:
+                    php_conf_file.write(php_conf)
 
-                    with open('/etc/apache2/apache2.conf', 'a') as php_conf_file:
-                        php_conf_file.write(php_conf)
-                """
                 """SSSFileUtils.create_symlink(self, ['/etc/phpmyadmin/apache.conf','/etc/apache2/apache2.conf'])"""
 
-                sss_php = open('/etc/apache2/apache2.conf',encoding='utf-8', mode='a')
+                """sss_php = open('/etc/apache2/apache2.conf',encoding='utf-8', mode='a')
                 self.app.render((data), 'Include /etc/phpmyadmin/apache.conf',
                                 out=sss_php)
                 sss_php.close()
-
+                """
             if set(SSSVariables.sss_php).issubset(set(apt_packages)):
                 # Create log directories
                 if not os.path.exists('/var/log/php/7.0/'):
